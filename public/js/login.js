605c1c279dcf51f264a993dac2b6b7425d9e4112
vinaykumar1131 <veenuk1131@gmail.com>
2023-05-13 20:08:46 +0530

    .....

--- public/js/login.js
+++ public/js/login.js
@@ -1,70 +1,72 @@
 // const axios=require('axios')
 import axios from 'axios';
 
-console.log("yes we got it");
+console.log('yes we got it');
 // console.log(f);
 
 export const login = async function (email, password) {
   try {
-    const res = await axios({
-      method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/user/login',
-      data: {
-        email,
-        password
-      }
-    }, { withCredentials: true });
+    const res = await axios(
+      {
+        method: 'POST',
+        url: '/api/v1/user/login',
+        data: {
+          email,
+          password,
+        },
+      },
+      { withCredentials: true }
+    );
 
-    if (res.data.status === "Success") {
-      alert("Login Successfully");
+    if (res.data.status === 'Success') {
+      alert('Login Successfully');
       window.setTimeout(() => {
-        location.assign('/')
+        location.assign('/');
       }, 1000);
     }
   } catch (err) {
     console.log(err);
   }
-}
-export const signup = async function (name, email,password,conformpass) {
+};
+export const signup = async function (name, email, password, conformpass) {
   try {
-    const res = await axios({
-      method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/user/signup',
-      data: {
-        name,
-        email,
-        password,conformpass
-      }
-    }, { withCredentials: true });
+    const res = await axios(
+      {
+        method: 'POST',
+        url: '/api/v1/user/signup',
+        data: {
+          name,
+          email,
+          password,
+          conformpass,
+        },
+      },
+      { withCredentials: true }
+    );
 
-    if (res.data.status === "Success") {
-      alert("Login Successfully");
+    if (res.data.status === 'Success') {
+      alert('Login Successfully');
       window.setTimeout(() => {
-        location.assign('/')
+        location.assign('/');
       }, 1000);
-
-
     }
-
-
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/user/logout',
-    })
+      url: '/api/v1/user/logout',
+    });
     if (res.data.status === 'Success') {
       location.reload(true);
     }
   } catch (err) {
-    console.log(err.response)
-    window.alert("some error in logout")
-    console.log("some error is there")
+    console.log(err.response);
+    window.alert('some error in logout');
+    console.log('some error is there');
   }
-}
-
+};
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,9 +5,7 @@ const stripe = Stripe(
 );
 export const bookTour = async (tourId) => {
   try {
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/booking/checkout-session/${tourId}`);
 
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
